f4844c5ba28ae3fc8bc2e86e0ab61bbb5743c1f0
Gaurav Rajput <rajput.gaurav8341@gmail.com>
2026-06-03 22:30:37 +0530

    Time: 58 ms (38.86%), Space: 27.8 MB (72.69%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,31 +1,27 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        # basicaaly we make this list by trimming the tree at the leaves until there are only 2 elemnts or less than 2 elements in th tree
-
-        if n == 1:
-            return [0]# [i for i in range(n)]
-
-        adj_map = defaultdict(list)
-        degree = [0] * n
-        for n1, n2 in edges:
-            adj_map[n1].append(n2)
-            adj_map[n2].append(n1)
-            degree[n1] += 1
-            degree[n2] += 1
         
-        # get all the leaves
-        leaves = deque([i for i in range(n) if degree[i] == 1])
+        if n == 1:
+            return [0]
+            
+        adj_map = defaultdict(list)
+        degrees = [0] * n 
+        for u, v in edges:
+            adj_map[u].append(v)
+            adj_map[v].append(u)
+            degrees[u] += 1
+            degrees[v] += 1
+        
+        leaves = deque([l for l in range(len(degrees)) if degrees[l] == 1])
 
-        remaining_nodes = n
-        while remaining_nodes > 2:
-            leaves_count = len(leaves)
-            remaining_nodes -= leaves_count
-
-            for _ in range(leaves_count):
+        while n > 2:
+            l_count = len(leaves)
+            n = n - l_count
+            for _ in range(l_count):
                 leaf = leaves.popleft()
                 for nei in adj_map[leaf]:
-                    degree[nei] -= 1
-                    if degree[nei] == 1:
+                    degrees[nei] -= 1
+                    if degrees[nei] == 1:
                         leaves.append(nei)
-            
+
         return list(leaves)
